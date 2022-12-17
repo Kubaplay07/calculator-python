c91d0c99edc95a50810c6d16a05f49c7f546608d
Kuba <67916528+Kubaplay07@users.noreply.github.com>
2022-12-17 22:07:39 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from decimal import Decimal
 
 class Calculator:
     def __init__(self, root):
@@ -45,61 +46,87 @@ class Calculator:
         self.button_8 = tk.Button(self.number_buttons_frame, text='8', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_number('8'))
         self.button_8.grid(row=0, column=1)
 
-        self.button_9 = tk.Button(self.number_buttons_frame, text='9', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_number('9')) 
+        self.button_9 = tk.Button(self.number_buttons_frame, text='9', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_number('9'))
         self.button_9.grid(row=0, column=2)
 
         self.button_0 = tk.Button(self.number_buttons_frame, text='0', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_number('0'))
-        self.button_0.grid(row=3, column=0)
+        self.button_0.grid(row=3, column=1)
 
         self.button_decimal = tk.Button(self.number_buttons_frame, text='.', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_number('.'))
-        self.button_decimal.grid(row=3, column=1)
-
-        self.button_clear = tk.Button(self.number_buttons_frame, text='C', bg='#424242', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.clear_display())
-        self.button_clear.grid(row=3, column=2)
+        self.button_decimal.grid(row=3, column=2)
 
         # Initialize the operator buttons frame
         self.operator_buttons_frame = tk.Frame(self.root, bg='#212121', height=300, width=75)
         self.operator_buttons_frame.pack(side='right')
 
-        # Initialize the operator buttons
-        self.button_divide = tk.Button(self.operator_buttons_frame, text='/', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_operator('/'))
-        self.button_divide.pack(side='top')
+ 
 
-        self.button_subtract = tk.Button(self.operator_buttons_frame, text='-', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_operator('-'))
-        self.button_subtract.pack(side='top')
+ # Initialize the operator buttons
+        self.button_add = tk.Button(self.operator_buttons_frame, text='+', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.set_operator('+'))
+        self.button_add.grid(row=0, column=0)
 
-        self.button_add = tk.Button(self.operator_buttons_frame, text='+', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.append_operator('+'))
-        self.button_add.pack(side='top')
+        self.button_subtract = tk.Button(self.operator_buttons_frame, text='-', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.set_operator('-'))
+        self.button_subtract.grid(row=1, column=0)
 
-        self.button_equals = tk.Button(self.operator_buttons_frame, text='=', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.calculate())
-        self.button_equals.pack(side='bottom')
+        self.button_multiply = tk.Button(self.operator_buttons_frame, text='*', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.set_operator('*'))
+        self.button_multiply.grid(row=2, column=0)
+
+        self.button_divide = tk.Button(self.operator_buttons_frame, text='/', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=lambda: self.set_operator('/'))
+        self.button_divide.grid(row=3, column=0)
+
+        # Initialize the result button
+        self.button_result = tk.Button(self.number_buttons_frame, text='=', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=self.get_result)
+        self.button_result.grid(row=3, column=0)
+
+        # Initialize the clear button
+        self.button_clear = tk.Button(self.number_buttons_frame, text='C', bg='#212121', fg='#FFFFFF', font=('Helvetica', 18), width=5, height=2, command=self.clear)
+        self.button_clear.grid(row=3, column=1)
+
+        # Initialize variables for storing the operands and the operator
+        self.operand1 = Decimal(0)
+        self.operand2 = Decimal(0)
+        self.operator = ''
+        self.result = Decimal(0)
+        self.current_operand = ''
 
     def append_number(self, number):
-        # Append the number to the display
-        current_text = self.display.cget('text')
-        if current_text == '0':
-            self.display.config(text=number)
-        else:
-            self.display.config(text=current_text + number)
+        self.current_operand += number
+        self.display.config(text=self.current_operand)
 
-    def append_operator(self, operator):
-        # Append the operator to the display
-        current_text = self.display.cget('text')
-        self.display.config(text=current_text + operator)
+    def set_operator(self, operator):
+        self.operator = operator
+        self.operand1 = Decimal(self.current_operand)
+        self.current_operand = ''
 
-    def clear_display(self):
-        # Reset the display to zero
+    def get_result(self):
+        self.operand2 = Decimal(self.current_operand)
+
+        if self.operator == '+':
+            self.result = self.operand1 + self.operand2
+        elif self.operator == '-':
+            self.result = self.operand1 - self.operand2
+        elif self.operator == '*':
+            self.result = self.operand1 * self.operand2
+        elif self.operator == '/':
+            self.result = self.operand1 / self.operand2
+
+        self.display.config(text=str(self.result))
+        self.operand1 = self.result
+        self.current_operand = ''
+
+    def clear(self):
+        self.operand1 = Decimal(0)
+        self.operand2 = Decimal(0)
+        self.operator = ''
+        self.result = Decimal(0)
+        self.current_operand = ''
         self.display.config(text='0')
 
-    def calculate(self):
-        # Calculate the result of the expression
-        try:
-            result = eval(self.display.cget('text'))
-            self.display.config(text=str(result))
-        except Exception:
-            self.display.config(text='Error')
+# Create the root window
+root = tk.Tk()
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    calculator = Calculator(root)
-    root.mainloop()
+# Create the calculator
+calculator = Calculator(root)
+
+# Run the main loop
+root.mainloop()
